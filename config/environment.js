a123705f687c16b40c83914739afe0707a58692f
580124 <taeyoon@C02NK0ZGG3QT.local>
2015-07-13 11:34:24 -0400

    added pods as default with a example post pod

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'tae-ember2',
+    podModulePrefix: 'tae-ember2/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
